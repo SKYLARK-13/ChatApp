56828d40052ee7228a98428ce44025679717bb31
Vikas Dubey <vikasdubeysu9090@gmail.com>
2021-06-05 09:48:13 +0530

    final

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyCq_UwQno17WwCESreGPB5PBVlCONWkGTE",
-  authDomain: "whatsapp-clone-904e4.firebaseapp.com",
-  projectId: "whatsapp-clone-904e4",
-  storageBucket: "whatsapp-clone-904e4.appspot.com",
-  messagingSenderId: "1081165565679",
-  appId: "1:1081165565679:web:58d6e267cbdb46843c951a",
+  //configurate by personal firebase
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
